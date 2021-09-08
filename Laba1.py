7b98fe6523fb0a4756f4ac4d3f3d852bacc4522d
destroysus <90268610+destroysus@users.noreply.github.com>
2021-09-08 21:35:00 +0500

    Добавил меню программы

--- Laba1.py
+++ Laba1.py
@@ -1,12 +1,26 @@
-a=float(input())
-b=float(input())
-c=a+b
-d=a-b
-e=abs((a+b))/2
-g=a*b
-h=a/b
-print("Сумма =",c)
-print("Разность =", d)
-print("Среднее арифметическое =", e)
-print("Произведение =", g)
-print("Частное =", h)
+status = -1
+spisok = []
+sotr = []
+
+while status != 2:
+    print('Введите одну из следующих цифр для вызова одной из описанных команд:')
+    print('0 - Открыть программу')
+    print('1 - Закрыть программу')
+    status = int(input())
+    print(' ')
+    if status == 0:
+        print('Введите два числа')
+        a=float(input())
+        b=float(input())
+        c=a+b
+        d=a-b
+        e=abs((a+b))/2
+        g=a*b
+        h=a/b
+        print('Ответы:')
+        print("Сумма =",c)
+        print("Разность =", d)
+        print("Среднее арифметическое =", e)
+        print("Произведение =", g)
+        print("Частное =", h)
+
